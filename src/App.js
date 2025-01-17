31fe4aa1bb8dc8e042899c5605344e74a4312f77
ShubhangiSinghal18 <shubhangisinghal550@gmail.com>
2025-01-18 01:20:02 +0530

    updated context functionalities

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 
+import { useState } from 'react';
 import './App.css';
+import { TodoProvider } from './context/TodoContext';
 
 function App() {
+const [todos, settodos] = useState([])
+const addTodo= (todo) =>{
+  // settodos(todo)   //will delete all the existing values and initialize todo to this new value
+  settodos((prev)=>[{id:Date.now(),...todo},...prev])
+}
+const updatedTodo=(id, todo)=>{
+  settodos((prev)=> prev.map((prevTodo)=>(prevTodo.id===id?todo:prevTodo)))
+}
+const deleteTodo=(id)=>{
+  settodos((prev)=>prev.filter((todo)=>todo.id!==id))
+}
+const toggleComplete=(id) =>{
+settodos((prev)=>prev.map((prevTodo)=> prevTodo===id?{...prevTodo,completed:!prevTodo.completed}:prevTodo))
+}
+
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <TodoProvider value={{todos,addTodo,updatedTodo,deleteTodo,toggleComplete}}>    
+     <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                       
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
   );
 }
 
