ee6fdfe5a94a0f2534e05a463e8a22e50a7d24d2
ShubhangiSinghal18 <shubhangisinghal550@gmail.com>
2025-01-18 11:35:09 +0530

    crud operations on todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import { TodoProvider } from './context/TodoContext';
+import { TodoForm, TodoItem } from './components';
 
 function App() {
 const [todos, settodos] = useState([])
@@ -16,8 +17,22 @@ const deleteTodo=(id)=>{
   settodos((prev)=>prev.filter((todo)=>todo.id!==id))
 }
 const toggleComplete=(id) =>{
-settodos((prev)=>prev.map((prevTodo)=> prevTodo===id?{...prevTodo,completed:!prevTodo.completed}:prevTodo))
+settodos((prev)=>prev.map((prevTodo)=> prevTodo.id===id?{...prevTodo,completed:!prevTodo.completed}:prevTodo))
 }
+
+useEffect(() => {
+  const todos = JSON.parse(localStorage.getItem("todos"))
+
+  if(todos && todos.lenth >0) {
+      settodos(todos)
+  }
+}, [])
+
+useEffect(() => {
+  localStorage.setItem("todos" , JSON.stringify(todos))
+}, [todos])
+
+
 
   return (
     <TodoProvider value={{todos,addTodo,updatedTodo,deleteTodo,toggleComplete}}>    
@@ -26,10 +41,16 @@ settodos((prev)=>prev.map((prevTodo)=> prevTodo===id?{...prevTodo,completed:!pre
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
-                       
+                       <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                          <div key={todo.id} className='w-full'>
+                              <TodoItem todo={todo }/>
+
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
